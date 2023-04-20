73b8d5115b6d741e1355fa48fd41150d08757580
MOHAMMED SALIK S B <129723817+SALIK2004@users.noreply.github.com>
2023-04-20 12:28:06 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Footer.css'
+const Footer=()=>{
+    return(
+        <div className="s">
+            <h3>~MOHAMMED SALIK S B</h3>
+            <h3>~727721euee048</h3>
+        </div>
+    )
+}
+export default Footer;
---
+++ Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Link} from "react-scroll"
+import "./Header.css"
+const Header=()=>{
+    return(
+        <div className='header'>
+            <div className='header__left'>
+                <h1>PORT<span>FOLIO</span></h1>
+            </div>
+            <div className='header__right'>
+                <Link to="about" smooth={true} duration={500}>
+                    <h4>
+                        ABOUT ME
+                    </h4>
+                </Link>
+                <Link to="skills" smooth={true} duration={500}>
+                    <h4>
+                        SKILLS
+                    </h4>
+                </Link>
+                <Link to="contact" smooth={true} duration={500}>
+                    <h4>
+                        CONTACT
+                    </h4>
+                </Link>
+            </div>
+        </div>
+    )
+}
+export default Header;
